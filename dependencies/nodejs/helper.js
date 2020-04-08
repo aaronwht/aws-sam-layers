07713f3e0de758dab54f52dd5481bb7aacf65f13
Aaron White <aaron@aaron.local>
2020-04-08 09:46:53 -0700

    Initial Commit

---
+++ dependencies/nodejs/helper.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken")
+module.exports.getToken = (event) => {
+  try {
+    const payload = { randomKey: Math.random(), exp: Math.floor(new Date().getTime() / 1000) + 1200, iat: Date.now() }
+    return jwt.sign(payload, "someKey")
+  } catch (err) {
+    console.error(err)
+    return { error: true }
+  }
+};
---
+++ extend/index.js
@@ -0,0 +1,15 @@
+"use strict";
+const utils = require("/opt/nodejs/helper.js");
+
+module.exports.handler = async (event, context) => {
+  
+  const  token = utils.getToken();
+  
+  return {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ token }),
+  };
+};
